821466964aa30548083a4add08443c52d4ab3305
Nihar <nihar1819@gmail.com>
2019-12-14 18:49:17 +0530

    Create datatype.py

---
+++ datatype.py
@@ -0,0 +1,33 @@
+
+
+# integer datatype
+
+variable_name = 10
+number = 20
+salary = 120000
+
+
+# string datatype
+
+variable_name = 'value'
+name = 'Dheeren'
+my_name = "Dheeren"
+
+your_name = "Nihar Ranjan"
+
+# List datatype
+
+variable_name = [1,2,3]
+my_items = ['mobile', 'house', 'bike']
+student = [105, 'nihar', 2.5]
+
+# tuple datatype
+
+variable_name = (1,2,3)
+my_items = ('mobile', 'house', 'bike')
+student = (105, 'nihar', 2.5)
+
+
+# dictionary datatype
+
+variable_name = {'name': 'Dheeren', 'mob': 123543233, 'salary': 4349830.90}
